78d5717504dd755fadde1072cc355ab12552fc92
sebasdelr <sebasdelr@gmail.com>
2021-05-03 19:08:06 -0700

    Small fixes

--- src/components/Items.js
+++ src/components/Items.js
@@ -8,9 +8,10 @@ function Items(props){
     
     return(
         <div className="item-menu">
-            <NoteItem title={list[0].title} />
-            <NoteItem title={list[1].title}/>
-            <NoteItem title={list[2].title}/>
+            {list.map(note => 
+                <NoteItem title= {note.title} 
+            />)}
+
         </div>
 
 
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,13 +1,21 @@
 import "./Items.css";
 
+import React, { useState } from "react";
+
 // import "./fontawesome-all.min.css";
 
 
 
 const NoteItem = (props) => {
 
+    const[isSelected, setSelected] = useState('false');
+
+    const toggleSelect = () => {
+        !isSelected ? setSelected(true) : setSelected(false); 
+    }
+
     return(
-    <div className='note-item-title'>
+    <div onClick={toggleSelect} className={` ${isSelected ? 'note-item-selected' : ''} `} >
         <p><span className="icon solid fa-book-open"></span>{props.title}</p>
     </div>
 
