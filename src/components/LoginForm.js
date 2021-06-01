7413cf61339247b430116ea93b9f9e4e65431ff8
sebasdelr <sebasdelr@gmail.com>
2021-05-31 22:09:19 -0700

    Added some more login handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,42 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import "./LoginForm.css";
 
-
 const LoginForm = () => {
-  const [isValid, setValid] = useState(false);
-  
-  const modalCloseHandler = (event) => {
-    event.preventDefault();
-    setValid(true);
-  }
 
-  const showForm = (    
-  <div>
-    <div className="backdrop"></div>
-    <div className="login-form">
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-      <form onSubmit={modalCloseHandler}>
-        <label>Username</label>
-        <input type="text" placeholder="Enter Username"></input>
-        <label>Password</label>
-        <input type="password" placeholder="Enter Password"></input>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+	
 
-    
-  </div>
-  );
+	useEffect(() => {
+		const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+		if(storedUserLoggedInInformation === '1') {
+		setIsLoggedIn(true);
+		}
+	}, []);
+
+	const modalCloseHandler = (event) => {
+		event.preventDefault();
+		const userName = userInputRef.current.value;
+		const passWord = passInputRef.current.value;
+
+		if(userName === "user" && passWord ==="pass") {
+			localStorage.setItem('isLoggedIn', '1');
+			setIsLoggedIn(true);
+		}
+		else{
+		console.log("invalid credentials");
+		}
 
 
-  return (
-    <div>
-      {!isValid ? showForm : ''}
-    </div>
+	};
 
-    
-  );
+	const logoutHandler = () => {
+		localStorage.removeItem('isLoggedIn');
+		setIsLoggedIn(false);
+	};
+
+	const userInputRef = useRef();
+	const passInputRef = useRef();
+
+
+
+
+	const showForm = (
+		<div>
+			<div className="backdrop"></div>
+			<div className="login-form">
+				<form onSubmit={modalCloseHandler}>
+					<label>Username</label>
+					<input
+						id="username"
+						type="text"
+						placeholder="Enter Username"
+						ref={userInputRef}
+					></input>
+					<label>Password</label>
+					<input
+						id="password"
+						type="password"
+						placeholder="Enter Password"
+						ref={passInputRef}
+					></input>
+					<button type="submit">Login</button>
+				</form>
+			</div>
+		</div>
+	);
+
+	return <div>{!isLoggedIn ? showForm : ""}</div>;
 };
 
 export default LoginForm;
