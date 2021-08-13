7d038d88858006b87d29f889f1f488a500975615
Sebastián Del Rincón <sebasdelr@gmail.com>
2021-08-12 23:29:12 -0700

    Added form validation

--- src/components/UI/Notes/NoteForm.js
+++ src/components/UI/Notes/NoteForm.js
@@ -1,12 +1,32 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 const NoteForm = (props) => {
 
+    const [showAlert, setShowAlert] = useState(false);
+
     const titleRef = useRef();
     const contentRef = useRef();
+
+    const newNote = () => { 
+        //can use ref as well
+        document.getElementById("note-form").reset();
+    }
+
+    const checkIfEmpty = title => {
+        if(title.trim().length === 0) {
+            
+            return false;
+            
+        }
+        else {
+            
+            return true;
+        }
+    }
 
     const submitHandler = event => {
         event.preventDefault();
@@ -14,14 +34,27 @@ const NoteForm = (props) => {
         const enteredTitle = titleRef.current.value;
         const enteredContent = contentRef.current.value;
 
-        const enteredText = {title: enteredTitle, content: enteredContent};
+        if(checkIfEmpty(enteredTitle)) {
+            setShowAlert(false);
+            const enteredText = {title: enteredTitle, content: enteredContent};
 
-        props.passNoteHandler(enteredText);
+            props.passNoteHandler(enteredText);
+
+        } else {
+            setShowAlert(true);
+            console.log("title is empty");
+        }
+
+        newNote();
+
+
 
     }
 
     return (
-        <Form  onSubmit={submitHandler}>
+        <Form  onSubmit={submitHandler} id="note-form">
+            
+            <Button variant="light" onClick={newNote}>New Note</Button>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Note Title</Form.Label>
                 <Form.Control type="input" ref={titleRef} />
@@ -30,7 +63,10 @@ const NoteForm = (props) => {
                 <Form.Label>Note Content</Form.Label>
                 <Form.Control as="textarea" ref={contentRef} rows={3} />
             </Form.Group>
-            <Button  type="submit">Save Note</Button>
+            {showAlert && <Alert variant="danger" >Please enter a title.</Alert>}
+            <Button  type="submit" variant="light" >Save Note</Button>
+            
+            
         </Form>
     );
 
