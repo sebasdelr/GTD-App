8c75fc9f412a68d5daf51f36bef8d265ae34695d
Sebastián Del Rincón <sebasdelr@gmail.com>
2021-08-21 14:52:25 -0700

    Updated item selection based on adding, editing, deleting notes

--- src/App.js
+++ src/App.js
@@ -81,30 +81,41 @@ const App = () => {
 
   const [noteCapture, setNoteCapture] = useState(notes);
 
-  const [useTitle, setTitle] = useState(notes[0].title);
-  const [useContent, setContent] = useState(notes[0]);
+  const [useContent, setContent] = useState(noteCapture[0]);
+  const [useIndex, setIndex] = useState(0);
 
-  const sendTitle = (title) => {
-    setTitle(title);
-  }
+
 
   const selectedItem = (content) => {
-    console.log(content);
     setContent(content);
+    setIndex(noteCapture.findIndex(element => element.id == content.id ));
   }
 
   const addNoteHandler = enteredText => {
     setNoteCapture(prevNotes => {
       const updatedNotes = [...prevNotes];
-      updatedNotes.unshift({ id: Math.random().toString(), title: enteredText.title, content: enteredText.content, date: new Date(2021, 5, 12) });
-      console.log('updated note');
-      return updatedNotes;
+      if(noteCapture.findIndex(element => element.id == enteredText.id ) >= 0) {
+        updatedNotes[useIndex] = { id: enteredText.id, title: enteredText.title, content: enteredText.content, date: new Date(2021, 5, 12) }
+        setContent(updatedNotes[useIndex])
+        return updatedNotes;
+        
+        
+      } else {
+        
+        updatedNotes.unshift({ id: enteredText.id, title: enteredText.title, content: enteredText.content, date: new Date(2021, 5, 12) });
+        setContent(updatedNotes[0])
+        return updatedNotes;
+        
+        
+      }
+      
     });
   };
 
   const deleteNoteHandler = noteId => {
     setNoteCapture(prevNotes => {
       const updatedNotes = prevNotes.filter(note => note.id !== noteId);
+      setContent(updatedNotes[useIndex])
       return updatedNotes;
     });
   };
@@ -147,7 +158,7 @@ const App = () => {
                   
                   <Col>
                     {/* <NoteContent title={useTitle} content={useContent} onAddNote={addNoteHandler}/> */}
-                    <NoteContent content={useContent} onAddNote={addNoteHandler}/>
+                    <NoteContent indexOfNote={useIndex} content={useContent} onAddNote={addNoteHandler}/>
 
 
                   </Col>
--- src/components/UI/Notes/NoteForm.js
+++ src/components/UI/Notes/NoteForm.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -7,13 +7,29 @@ import Alert from 'react-bootstrap/Alert';
 const NoteForm = (props) => {
 
     const [showAlert, setShowAlert] = useState(false);
+    const [useCanEdit, setCanEdit] = useState(true);
 
     const titleRef = useRef();
+    //usestate instead? useeffect does not rerender!
+    
     const contentRef = useRef();
+    const idRef = useRef();
+
+    useEffect(() => {
+        document.getElementById("note-title").value = props.selectedItem.title;
+        document.getElementById("note-content").value = props.selectedItem.content;
+        document.getElementById("note-id").value = props.selectedItem.id;
+    }, [props]);
 
     const newNote = () => { 
         //can use ref as well
         document.getElementById("note-form").reset();
+        document.getElementById("note-id").value = Math.random().toString();
+        setCanEdit(false);
+    }
+
+    const editNoteHandler = () => {
+        setCanEdit(true);
     }
 
     const checkIfEmpty = title => {
@@ -28,15 +44,21 @@ const NoteForm = (props) => {
         }
     }
 
+    
+
+    
+
     const submitHandler = event => {
         event.preventDefault();
         
         const enteredTitle = titleRef.current.value;
         const enteredContent = contentRef.current.value;
+        const generatedId = idRef.current.value;
+
 
         if(checkIfEmpty(enteredTitle)) {
             setShowAlert(false);
-            const enteredText = {title: enteredTitle, content: enteredContent};
+            const enteredText = {id: generatedId, title: enteredTitle, content: enteredContent, editable: useCanEdit};
 
             props.passNoteHandler(enteredText);
 
@@ -45,7 +67,7 @@ const NoteForm = (props) => {
             console.log("title is empty");
         }
 
-        newNote();
+        // newNote();
 
 
 
@@ -55,13 +77,20 @@ const NoteForm = (props) => {
         <Form  onSubmit={submitHandler} id="note-form">
             
             <Button variant="light" onClick={newNote}>New Note</Button>
+            <Button variant="light" onClick={editNoteHandler}>Edit Note</Button>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                <Form.Label>Note Id</Form.Label>
+                <Form.Control id="note-id" type="input" ref={idRef} readOnly/>
+            </Form.Group>
+
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Note Title</Form.Label>
-                <Form.Control type="input" ref={titleRef} />
+                <Form.Control id="note-title" type="input" ref={titleRef} />
             </Form.Group>
+            
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Note Content</Form.Label>
-                <Form.Control as="textarea" ref={contentRef} rows={3} />
+                <Form.Control id="note-content" as="textarea" ref={contentRef} rows={3} />
             </Form.Group>
             {showAlert && <Alert variant="danger" >Please enter a title.</Alert>}
             <Button  type="submit" variant="light" >Save Note</Button>
