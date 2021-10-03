185c04b4e9726bbcdf867406cd669fd02cbbf58d
Sebastián Del Rincón <sebasdelr@gmail.com>
2021-10-02 23:14:52 -0700

    Added filters for week and month

--- src/components/UI/Actions/ActionList.js
+++ src/components/UI/Actions/ActionList.js
@@ -24,7 +24,45 @@ const ActionList = () => {
         );
     };
 
-    const listHandler = () => {
+    const compareDateWeek = newDate => {
+
+        const WEEK = 7;
+        const DAY = 1;
+        const today = new Date();
+        // const thisWeek = new Date(today);
+        const thisWeek = new Date();
+        thisWeek.setDate(thisWeek.getDate() + WEEK);
+
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + DAY);
+
+
+        return (
+            newDate >= today &&
+            newDate < thisWeek
+        );
+    };
+
+    const compareDateMonth = newDate => {
+
+        const MONTH = 1;
+        const DAY = 1;
+        const today = new Date();
+        // const thisWeek = new Date(today);
+        const thisMonth = new Date();
+        thisMonth.setMonth(thisMonth.getMonth() + MONTH); 
+
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + DAY);
+
+
+        return (
+            newDate >= today &&
+            newDate < thisMonth
+        );
+    };
+
+    const listHandlerToday = () => {
         const actionOnlyList = notesCtx.items.filter(item => (item.type === "4" && compareDateToday(item.dateDue)));
 
 
@@ -52,14 +90,74 @@ const ActionList = () => {
             );
 
         }
-    }
+    };
+
+    const listHandlerWeek = () => {
+        const actionOnlyList = notesCtx.items.filter(item => (item.type === "4" && compareDateWeek(item.dateDue)));
+
+
+        if(actionOnlyList.length > 0) {
+            return (
+                actionOnlyList.map((action) => {
+                   
+
+                    return (
+                        <ActionItem key={action.id}
+                            
+                        actionItem={action}
+
+                        
+                    />);
+
+
+                    
+
+                })
+            );
+        } else {
+            return (
+                <p>No Items Found</p>
+            );
+
+        }
+    };
+
+    const listHandlerMonth = () => {
+        const actionOnlyList = notesCtx.items.filter(item => (item.type === "4" && compareDateMonth(item.dateDue)));
+
+
+        if(actionOnlyList.length > 0) {
+            return (
+                actionOnlyList.map((action) => {
+                   
+
+                    return (
+                        <ActionItem key={action.id}
+                            
+                        actionItem={action}
+
+                        
+                    />);
+
+
+                    
+
+                })
+            );
+        } else {
+            return (
+                <p>No Items Found</p>
+            );
+
+        }
+    };
 
     return (
         <React.Fragment>
             <Row>
                 <Col>
                     <h5>Today</h5>
-                    {listHandler()}
+                    {listHandlerToday()}
                 </Col>
 
                 
@@ -68,7 +166,7 @@ const ActionList = () => {
 
                 <Col>
                     <h5>This Week</h5>
-                    {listHandler()}
+                    {listHandlerWeek()}
                 </Col>
 
                 
@@ -77,7 +175,7 @@ const ActionList = () => {
 
                 <Col>
                     <h5>This Month</h5>
-                    {listHandler()}
+                    {listHandlerMonth()}
                 </Col>
                 
             </Row>
